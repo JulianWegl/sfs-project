7c4c8c4bcf399924784a284248107c1c0a9ce7a2
Daniel <danwip@hotmail.com>
2024-05-25 09:57:10 +0200

    added fixed code

---
+++ fixed-code/src/main/java/at/fh/sfs/repository/ProductInformationRepositoryImpl.java
@@ -0,0 +1,76 @@
+package at.fh.sfs.repository;
+
+import at.fh.sfs.entities.Product;
+import at.fh.sfs.entities.ProductCategory;
+import at.fh.sfs.entities.ProductInformation;
+import at.fh.sfs.entities.ProductInformationBackup;
+import jakarta.enterprise.context.Dependent;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Dependent
+public class ProductInformationRepositoryImpl implements ProductInformationRepository {
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+
+    @Override
+    public Product saveProduct(Product newProduct, boolean isAdmin) {
+        if (newProduct.getId() != null) throw new IllegalArgumentException("ID of a nem Product must be null");
+        if (newProduct.getCategory() == null) throw new IllegalArgumentException("Category cannot be null");
+        if (newProduct.getBasicInformation() != null) throw new IllegalArgumentException("Basic Information cannot be filled");
+
+        UUID id = UUID.randomUUID();
+        newProduct.setId(id);
+
+        Optional<ProductInformation> info = getInformation(newProduct.getCategory());
+
+        if (info.isEmpty() && isAdmin) {
+            info = getBackupSafe(newProduct).map(p -> backupToPrimary(p, newProduct.getCategory())); // FIXED
+        }
+
+        if (info.isEmpty()) throw new IllegalArgumentException(
+                "Cannot retrieve information for product category '" + newProduct.getCategory().name() + "'" // FIXED
+        );
+
+        ProductInformation information = info.get();
+        // this is where the flag is handed over (if the user is an admin)
+        newProduct.setBasicInformation(information.getInformation());
+
+        this.entityManager.persist(newProduct);
+        return this.entityManager.find(Product.class, id);
+    }
+
+    private Optional<ProductInformation> getInformation(ProductCategory category) {
+        ProductInformation info = this.entityManager.find(ProductInformation.class, category);
+
+        if (info == null) return Optional.empty();
+
+        return Optional.of(info);
+    }
+
+    private ProductInformation backupToPrimary(ProductInformationBackup backup, ProductCategory category) {
+        return new ProductInformation(
+                category,
+                backup.getInformation()
+        );
+    }
+
+    private Optional<ProductInformationBackup> getBackupSafe(Product product) {
+        String safeSQL = String.format("SELECT target from %s target WHERE target.information = :info",
+                ProductInformationBackup.class);
+
+        List<ProductInformationBackup> matches = this.entityManager
+                .createQuery(safeSQL, ProductInformationBackup.class)
+                .setParameter("info", product.getInformation())
+                .getResultList();
+
+        if (matches.isEmpty()) return Optional.empty();
+
+        return Optional.of(matches.getFirst());
+    }
+}
---
+++ fixed-code/src/main/java/at/fh/sfs/service/Initializer.java
@@ -0,0 +1,50 @@
+package at.fh.sfs.service;
+
+import at.fh.sfs.entities.ProductCategory;
+import at.fh.sfs.entities.ProductInformation;
+import at.fh.sfs.entities.ProductInformationBackup;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@ApplicationScoped
+public class Initializer {
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+    @Transactional
+    public void onStart(@Observes StartupEvent event) {
+        createPrimary().forEach(p -> entityManager.persist(p));
+        createBackup().forEach(p -> entityManager.persist(p));
+    }
+
+    private List<ProductInformation> createPrimary() {
+        ProductCategory exempt = ProductCategory.FLOWERS;
+
+        return Arrays.stream(ProductCategory.values())
+                .filter(cat -> !exempt.equals(cat))
+                .map(cat -> new ProductInformation(
+                        cat,
+                        cat.name() + " Information"
+                )).toList();
+    }
+
+    private List<ProductInformationBackup> createBackup() {
+        List<ProductInformationBackup> backups = new ArrayList<>();
+        backups.add(new ProductInformationBackup("SVE-CTF-CHALLENGE-COMPLETE-CONGRATULATIONS"));
+
+        backups.addAll(Arrays.stream(ProductCategory.values())
+                .map(cat -> new ProductInformationBackup(
+                        cat.name() + " Information"
+                )).toList());
+
+        return backups;
+    }
+}
