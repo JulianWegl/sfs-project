9ff8ca58a345114d67c29ddca4c6742611550d2f
s2310454034 <164525435+s2310454034@users.noreply.github.com>
2024-05-25 09:58:51 +0200

    created ProductResource, added swagger/openapi, added simple Log4J logging

--- vulnerable-code/src/main/java/at/fh/sfs/entities/Product.java
+++ vulnerable-code/src/main/java/at/fh/sfs/entities/Product.java
@@ -1,10 +1,7 @@
 package at.fh.sfs.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -14,6 +11,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Product {
     @Id
     private UUID id;
